959a9cd0b56db28aa7a8441f110b2df52c1172c1
lucasrygielski <lucas.rygielski@gmail.com>
2020-08-14 14:58:22 +0200

    maj

--- site_service/site.py
+++ site_service/site.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from controlers.utilisateur_controler import UtilisateursControler
+
 site = Flask(__name__)
 
 CORS(site)
